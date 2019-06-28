5a9d85be4112eacd440e459b56fa9df344cfaac0
Syksy <teemu.daniel.laajala@gmail.com>
2019-06-29 00:33:20 +0300

    v0.1-2

--- HexEn/Hex.cs
+++ HexEn/Hex.cs
@@ -12,16 +12,62 @@ namespace HexEn3D
          *    \ /   \ /
          *  5. \-----/ 4.
          */
-        private xyz corner0;
-        private xyz corner1;
-        private xyz corner2;
-        private xyz corner3;
-        private xyz corner4;
-        private xyz corner5;
         // Additionally, for 3d plotting the mesh triangles require two additional points inside
-        private xyz corner6;
-        private xyz corner7;
+        private xyz[] vertices; // Length 8
+        // The height at which the hex is at
+        private double elevation;
+        // Whether the Hex is in active use
+        private Boolean active;
+
+        public Hex()
+        {
+            for(int i=0; i<8; i++)
+            {
+                vertices[i] = new xyz(); // coords 0,0,0
+            }
+            this.elevation = 0.0; // By default on the z=0 plane
+            this.active = true; // Created Hexes are active by default
+        }
+        public Hex(xyz[] xyzs, double elevation)
+        {
+            if (xyzs.Length != 8) throw new System.ArgumentOutOfRangeException("Parameter xyzs should be of length 8 in Hex class.");
+            for (int i = 0; i < 8; i++)
+            {
+                vertices[i] = new xyz(); // coords from the provided table
+            }
+            this.elevation = elevation; // Custom z-axis elevation
+            this.active = true; // Created Hexes are active by default
+        }
+
+        // Getters
+        public double getElevation()
+        {
+            return this.elevation;
+        }
+        public xyz[] getVertices()
+        {
+            return this.vertices;
+        }
+        public xyz getVertexAt(int i)
+        {
+            return this.vertices[i];
+        }
 
 
+        // Setters
+        public void setElevation(double elevation)
+        {
+            this.elevation = elevation;
+        }
+        public void setVertex(xyz xyztmp, int index)
+        {
+            if (index < 0 | index > 8) throw new System.ArgumentOutOfRangeException("Parameter index in Hyx.setVertex should be between 0 and 7.");
+            this.vertices[index] = xyztmp;
+        }
+        public void setVertex(xyz[] xyztmp)
+        {
+            if(xyztmp.Length!=8) throw new System.ArgumentOutOfRangeException("Parameter xyztmp in Hyx.setVertex should be a xyz[] array of length 8.");
+            this.vertices = xyztmp;
+        }
     }
 }
